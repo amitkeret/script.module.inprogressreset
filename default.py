f2e74e5d60dbda86d92a01aa2a0a9ac652aa9c4e
Amit Keret <amitkeretwork@gmail.com>
2013-11-25 21:19:09 +0200

    fixed quotes, imports

--- default.py
+++ default.py
@@ -1,5 +1,5 @@
 import sys
-import xbmc, xbmcgui, xbmcaddon
+import xbmc, xbmcaddon
 
 __addon__        = xbmcaddon.Addon()
 __addonid__      = __addon__.getAddonInfo('id')
@@ -14,20 +14,22 @@ def log(txt):
 class Main:
     def __init__(self):
         log('version %s started' % __addonversion__ )
+        
         self.DBID = xbmc.getInfoLabel('ListItem.DBID')
-        if self.DBID <> "":
+        if self.DBID <> '':
             self._inprogress_reset()
         else:
-            log("No DBID found")
-
+            log('No DBID found')
+    
     def _inprogress_reset(self):
         try:
             xbmc.executeJSONRPC('{"id": 1, "jsonrpc": "2.0", "method": "VideoLibrary.SetMovieDetails", "params": {"movieid": %s, "resume": {"position": 0, "total": 0}}}' % (self.DBID))
         except Exception:
-            log("unable to update in-progress state in DBID %s" %s (self.DBID))
+            log('unable to update in-progress state in DBID %s' %s (self.DBID))
             sys.exc_clear()
     
-if ( __name__ == "__main__" ):
+
+if ( __name__ == '__main__' ):
     Main()
 
 log('finished')
